cde57e4d83c76ac3e64f85c17ec970cd95800805
270205493 <270205493@qq.com>
2019-03-13 17:03:06 +0800

    添加断路器hystrix  spring-cloud-starter-netflix-hystrix 添加断路器仪表盘  spring-cloud-starter-netflix-hystrix-dashboard 仪表盘连接失败 通过在应用类中添加HystrixMetricsStreamServlet 解决

--- ribbon/src/main/java/com/lgw/ribbon/RibbonApplication.java
+++ ribbon/src/main/java/com/lgw/ribbon/RibbonApplication.java
@@ -1,10 +1,14 @@
 package com.lgw.ribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +16,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
 public class RibbonApplication {
 
 	public static void main(String[] args) {
@@ -23,5 +29,17 @@ public class RibbonApplication {
 	RestTemplate restTemplate(){
 		return  new RestTemplate();
 	}
+
+	@Bean
+	public ServletRegistrationBean getServlet() {
+
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		registrationBean.addUrlMappings("/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}
+
 
 }
--- ribbon/src/main/java/com/lgw/ribbon/service/LoadBalance.java
+++ ribbon/src/main/java/com/lgw/ribbon/service/LoadBalance.java
@@ -1,5 +1,6 @@
 package com.lgw.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,8 +10,13 @@ public class LoadBalance {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "logError")
     public String testBanlance(String name){
         return restTemplate.getForObject("http://examination/examination?name="+name,String.class);
+    }
+
+    public String logError(String name){
+        return name+"log error！";
     }
 
 }
