bc3a863a88493954cad311d4a45527d7907bee14
270205493 <270205493@qq.com>
2019-03-13 17:05:00 +0800

    feign添加断路器

--- feign/src/main/java/com/lgw/feign/client/SchedualServiceIsLogin.java
+++ feign/src/main/java/com/lgw/feign/client/SchedualServiceIsLogin.java
@@ -1,11 +1,12 @@
 package com.lgw.feign.client;
 
+import com.lgw.feign.SchedualServiceLogHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "examination")
+@FeignClient(value = "examination",fallback= SchedualServiceLogHystrix.class)
 public interface SchedualServiceIsLogin {
     @RequestMapping(value = "/examination",method = RequestMethod.GET)
     String IsLoginFromClientOne(@RequestParam(value = "name")String name);
